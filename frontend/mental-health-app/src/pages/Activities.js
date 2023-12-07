632a6f69d870f680a38517b147de799bd2aad08f
Koleton Murray <106117059+koletonmurray@users.noreply.github.com>
2023-12-06 23:44:22 -0700

    added extra activities

--- frontend/mental-health-app/src/pages/Activities.js
+++ frontend/mental-health-app/src/pages/Activities.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageTitle from '../components/PageTitle';
+import Blurb from '../components/Blurb';
 
 function Activities() {
 
@@ -28,6 +29,18 @@ function Activities() {
             "name": "Write a Letter",
             "description": "Write a letter to a family member or friend. You can send it later.",
             "example": "Dear mom, I love you."
+        },
+        "Gratitude Journaling":
+        {
+            "name": "Gratitude Journaling",
+            "description": "Take a few minutes today to write down at least 10 things you're grateful for.",
+            "example": "Today I am grateful for sunsets."
+        },
+        "Walk or Exercise":
+        {
+            "name": "Walk or Exercise",
+            "description": "Take a break from your routine to go for a walk or engage in light exercise. It can be refreshing and provide a mental and physical boost.",
+            "example": "A 15 minute peaceful walk to clear your mind."
         }
     }
 
@@ -35,17 +48,24 @@ function Activities() {
     const funfacts = activities["Fun Facts"];
     const meditation = activities["Meditation"];
     const letter = activities["Write a Letter"];
+    const gratitude = activities["Gratitude Journaling"];
+    const walk = activities["Walk or Exercise"];
 
     const [checkinSelected, setCheckinSelected] = React.useState(false);
     const [funfactsSelected, setFunfactsSelected] = React.useState(false);
     const [meditationSelected, setMeditationSelected] = React.useState(false);
     const [letterSelected, setLetterSelected] = React.useState(false);
+    const [gratitudeSelected, setGratitudeSelected] = React.useState(false);
+    const [walkSelected, setWalkSelected] = React.useState(false);
+
 
     function SelectCheckin() {
         checkinSelected ? setCheckinSelected(false) : setCheckinSelected(true);
         setFunfactsSelected(false);
         setMeditationSelected(false);
         setLetterSelected(false);
+        setGratitudeSelected(false);
+        setWalkSelected(false);
     }
 
     function SelectFunfacts() {
@@ -53,6 +73,8 @@ function Activities() {
         setCheckinSelected(false);
         setMeditationSelected(false);
         setLetterSelected(false);
+        setGratitudeSelected(false);
+        setWalkSelected(false);
     }
 
     function SelectMeditation() {
@@ -60,6 +82,8 @@ function Activities() {
         setCheckinSelected(false);
         setFunfactsSelected(false);
         setLetterSelected(false);
+        setGratitudeSelected(false);
+        setWalkSelected(false);
     }
 
     function SelectLetter() {
@@ -67,11 +91,33 @@ function Activities() {
         setCheckinSelected(false);
         setFunfactsSelected(false);
         setMeditationSelected(false);
+        setGratitudeSelected(false);
+        setWalkSelected(false);
+    }
+
+    function SelectGratitude() {
+        gratitudeSelected ? setGratitudeSelected(false) : setGratitudeSelected(true);
+        setCheckinSelected(false);
+        setFunfactsSelected(false);
+        setMeditationSelected(false);
+        setLetterSelected(false);
+        setWalkSelected(false);
+    }
+
+    function SelectWalk() {
+        walkSelected ? setWalkSelected(false) : setWalkSelected(true);
+        setCheckinSelected(false);
+        setFunfactsSelected(false);
+        setMeditationSelected(false);
+        setLetterSelected(false);
+        setGratitudeSelected(false);
     }
 
     return (
         <div>
-            <PageTitle title={'Activities'}/>
+            <PageTitle title={'Mental Health Activities'}/>
+
+            <Blurb>This dedicated space is designed with your well-being in mind. Explore engaging activities and games crafted to support and boost your mental health throughout your mission. These activites provide moments of connection, relaxation, stress relief, and self. Your mental well-being matters! Take a moment, explore, and let these activities nurture your emotional resilience.</Blurb> 
 
             <div className='activity-card'>
                 <div className={`activity-title ${checkinSelected ? 'selected' : ''}`} onClick={SelectCheckin}>
@@ -110,6 +156,26 @@ function Activities() {
                 <div className={`activity-description bg-lemon-50 ${letterSelected ? 'display-block' : 'display-none'}`}>
                     <p className='text-sunset-800'>{letter.description}</p>
                     <p className='activity-example'>"{letter.example}"</p>
+                </div>
+            </div>
+
+            <div className='activity-card'>
+                <div className={`activity-title ${gratitudeSelected ? 'selected' : ''}`} onClick={SelectGratitude}>
+                    <h1>{gratitude.name}</h1>
+                </div>
+                <div className={`activity-description bg-lemon-50 ${gratitudeSelected ? 'display-block' : 'display-none'}`}>
+                    <p className='text-sunset-800'>{gratitude.description}</p>
+                    <p className='activity-example'>"{gratitude.example}"</p>
+                </div>
+            </div>
+
+            <div className='activity-card'>
+                <div className={`activity-title ${walkSelected ? 'selected' : ''}`} onClick={SelectWalk}>
+                    <h1>{walk.name}</h1>
+                </div>
+                <div className={`activity-description bg-lemon-50 ${walkSelected ? 'display-block' : 'display-none'}`}>
+                    <p className='text-sunset-800'>{walk.description}</p>
+                    <p className='activity-example'>"{walk.example}"</p>
                 </div>
             </div>
 
