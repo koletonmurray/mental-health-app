961807a15b73648cf570797fe7fe5030912c155d
Koleton Murray <106117059+koletonmurray@users.noreply.github.com>
2023-12-09 13:39:31 -0700

    tried to fix scroll to top button

--- frontend/mental-health-app/src/components/ChatButton.js
+++ frontend/mental-health-app/src/components/ChatButton.js
@@ -1,6 +1,5 @@
 import React from "react";
-//import { Link } from "react-router-dom";
-import { BsChatSquareDots } from "react-icons/bs"; // You may need to install react-icons
+import { BsChatSquareDots } from "react-icons/bs";
 
 const ChatButton = () => {
   return (
@@ -10,7 +9,7 @@ const ChatButton = () => {
       rel="noopener noreferrer"
     >
       <div
-        className="fixed bottom-4 right-4 bg-lightgreen p-3 rounded-full text-white cursor-pointer"
+        className="fixed bottom-4 right-4 bg-lightgreen p-3 rounded-full text-white cursor-pointer z-50"
         title="Chat with an AI"
       >
         <BsChatSquareDots size={32} />
--- frontend/mental-health-app/src/layout/Layout.js
+++ frontend/mental-health-app/src/layout/Layout.js
@@ -5,16 +5,15 @@ import Footer from './Footer';
 
 function Layout({ children }) {
   return (
-    <div className="bg-gray-100 flex flex-col">
+    <div className="bg-gray-100 flex flex-col min-h-screen">
       <Header />
-      <div className="min-h-[650px] flex flex-col relative">
-        <div className="flex-grow p-4">
-          <div className="bg-white rounded-lg p-4 shadow-md" style={{contain: 'content', overflow: 'wrap'}}>{children}</div>
+      <div className="flex-grow p-4 relative">
+        <div className="bg-white rounded-lg p-4 shadow-md" style={{ contain: 'content', overflow: 'wrap' }}>
+          {children}
         </div>
-        {/* Add the ChatButton component at the bottom right corner */}
-        <ChatButton />
       </div>
       <Footer />
+      <ChatButton />
     </div>
   );
 }
